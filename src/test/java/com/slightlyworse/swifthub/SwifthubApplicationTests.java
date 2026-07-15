9a5564832c7a4fbf9878eff1c8d2ae2d94997159
nfpog <danhuynh307@gmail.com>
2026-07-14 20:56:17 -0400

    Initial commit: Spring Boot template

---
+++ src/test/java/com/slightlyworse/swifthub/SwifthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slightlyworse.swifthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwifthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
